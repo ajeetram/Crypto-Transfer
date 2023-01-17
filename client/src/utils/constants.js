6937f8f50e626bd65032eb391f605d7c09938a84
ajeetram <86039147+ajeetram@users.noreply.github.com>
2023-01-17 20:05:29 +0530

    client

---
+++ client/src/utils/constants.js
@@ -0,0 +1,7 @@
+import abi from "./Transactions.json";
+
+export const contractAddress = "0x038e813055DCA6990Dae94fE5074531714aEb8b0";
+export const contractABI = abi.abi;
+
+
+//Transactions address:  0x038e813055DCA6990Dae94fE5074531714aEb8b0
